04ded3f12f83d0162d1a60a7152d838f1c8c07ae
LapizdaSilva <volihabas@gmail.com>
2025-12-04 11:59:02 -0300

    Adicionado novamente chaves
    Foi adicionado as chaves do supabase para que possa ser possivel utilizar o aplicativo sem ter a necessidade de criar algo no supabase

--- supabase.ts
+++ supabase.ts
@@ -1,7 +1,9 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
+
+const SUPABASE_URL = 'https://mopvrodicskxiuuydapz.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vcHZyb2RpY3NreGl1dXlkYXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNDYwMDMsImV4cCI6MjA3NjYyMjAwM30.NOhQvnfyX-aPeqI0QT5m-obU_R8lvXzihU7VzUzq-DU'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
